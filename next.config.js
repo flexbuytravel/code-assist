6ae6af02adf0f8e495ef5d89809387eb6a35abab
Justin Earll <flexbuytravel@gmail.com>
2025-08-12 02:00:58 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  reactStrictMode: true,
+  swcMinify: true,
+  // Allow Next.js to run on localhost and your public IP
+  devIndicators: {
+    buildActivity: true
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*" // Allow all origins in dev mode
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
